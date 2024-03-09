24ae9e41bc6de7706790965c0b8a2c895d468656
Lateef Aminu <122064166+reallife01@users.noreply.github.com>
2024-03-09 15:12:28 +0100

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThirdwebProvider } from '@thirdweb-dev/react';
-import { CeloAlfajoresTestnet } from "@thirdweb-dev/chains";
+import { CeloAlfajoresTestnet } from "@thirdweb-dev/chains"; // make sure you import the testnet name 
 
 
 import { StateContextProvider } from './context';
@@ -13,8 +13,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <ThirdwebProvider 
-    activeChain={CeloAlfajoresTestnet}
-    clientId="05df97614128323f67280efed6e3613a">
+    activeChain={CeloAlfajoresTestnet} // make sure metamask is on celo Alfajores testnet
+    clientId="05df97614128323f67280efed6e3613a" // Don't touch here>
     <Router>
       <StateContextProvider>
         <App />
